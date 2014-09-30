20e408b875618f211e43c5e07f62d55add375b4c
Hao Zhong <zhhao31@gmail.com>
2014-09-30 15:29:39 -0400

    Remove inrelevant files.

--- Startups/views.py
+++
@@ -1,8 +0,0 @@
-# coding: UTF-8
-
-from django.shortcuts import render
-from django.http import HttpResponse
-from django import template
-
-def hello_view(request):
-	return HttpResponse(render(request, r"F:\Dropbox\Documents\Programming\Python\DjangoProjects\Startups\Startups\index.html", {}))
--- companies/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-import companies.models import CbCompanies
-
-# Register your models here.
-admin.site.register(CbCompanies)
