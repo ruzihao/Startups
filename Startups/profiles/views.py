3691efc572fa35c9c17c1652b804d5027b99ea8b
Hao Zhong <zhhao31@gmail.com>
2015-01-07 10:34:43 -0500

    add company_view, company_vidyo_view

--- Startups/profiles/views.py
+++ Startups/profiles/views.py
@@ -6,7 +6,13 @@ import os.path
 
 # Create your views here.
 def profiles_view(request):
+    return HttpResponse(render(request, "profiles/home.html", {}))
+
+def company_view(request):
     return HttpResponse(render(request, "profiles/company.html", {}))
+
+def company_vidyo_view(request):
+    return HttpResponse(render(request, "profiles/vidyo.html", {}))
 
 # def test_view(request):
 #     return HttpResponse(render(request, "profiles/test.html", {}))
