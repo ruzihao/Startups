daa921d5a52a689d9fa11c4e015f680caf6d2a45
adamzjw <adamzjw@msn.com>
2015-01-08 17:31:50 -0500

    add home path into url

--- Startups/Startups/urls.py
+++ Startups/Startups/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Startups.views.home', name='home'),
+    url(r'^$', home_view, name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^$', index_view, name='home'),
