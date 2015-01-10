36755068bb9997f6c0cb1c1492b393551106602c
adamzjw <adamzjw@msn.com>
2015-01-10 16:22:11 -0500

    Split views into several components
    make classes and JSON-perpare function as modules

---
+++ Startups/profiles/classes.py
@@ -0,0 +1,132 @@
+from profiles.obj2dict import object2dict
+
+class graph(object):
+	class node(object):
+	  def __init__(self, cid, name, match, description, invest_value):
+		   self.id = cid #indentified by cid
+		   self.name = name
+		   self.match = match
+		   self.description = description
+		   self.invest_value = invest_value
+		   self.url = "/profiles/%d" % cid
+	  def __repr__(self):
+		   return "Node %s with id %d<br>" % (self.name, self.id)
+
+	class edge(object):
+	  def __init__(self, source_cid, target_cid):
+		   self.source = source_cid   #indentified by cid
+		   self.target = target_cid   #indentified by cid
+	  def __repr__(self):
+		   return "Link from %d to %d<br>" % (self.source, self.target)
+
+	def __init__(self):
+	   self.nodes = []
+	   self.nodes_id = []
+	   self.links = []
+
+	def is_node_exist(self, new_node):
+	   return new_node.id in self.nodes_id
+
+	def is_edge_exist(self, new_edge):
+	   for link in self.links:
+			if link.source == new_edge.source and link.target == new_edge.target:
+				 return True
+	   else:
+			return False
+
+	def add_node(self, cid, name, match, description, invest_value):
+	   new_node = graph.node(cid, name, match, description, invest_value)
+	   if not self.is_node_exist(new_node):
+			self.nodes.append(new_node)
+			self.nodes_id.append(new_node.id)
+
+	def add_edge(self, source_cid, target_cid):
+	   new_edge = graph.edge(source_cid, target_cid)
+	   if not self.is_edge_exist(new_edge):
+			self.links.append(new_edge)
+			
+	def get_nodes(self):
+	   return self.nodes
+
+	def get_edges(self):
+	   return self.links
+
+	def __repr__(self):
+	   message = []
+	   message.append("Nodes<br>")
+	   for node in self.nodes:
+			message.append(str(node))
+	   message.append("Links<br>")
+	   for link in self.links:
+			message.append(str(link))
+	   return "".join(message)
+
+	def graph2dict(self):
+	   graph_dict = {"nodes":[], "links":[]}
+	   for node in self.get_nodes():
+			graph_dict["nodes"].append(object2dict(node))
+	   for link in self.get_edges():
+			graph_dict["links"].append(object2dict(link))
+	   return graph_dict
+	   
+	  
+
+class timeline(object):
+	class era(object):
+	  def __init__(self, headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d):
+		   self.startDate = "%d,%d,%d" % (start_y, start_m, start_d)
+		   if end_y == "":
+				self.endDate=",,,"
+		   else:
+				self.endDate = "%d,%d,%d" % (end_y, end_m, end_d)
+		   self.headline = headline
+		   self.text = "<p>%s</p>" % text
+		   self.tag = tag
+
+	class event(era):
+	  def __init__(self, headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d, classname, media, thumbnail, credit, caption):
+		   super(timeline.event, self).__init__(headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d)
+		   self.classname = classname
+		   self.asset = {
+						  "media": media,
+						  "thumbnail": thumbnail,
+						  "credit": credit,
+						  "caption": caption,
+						}
+						
+	def __init__(self, timeline_headline, timeline_type="default", timeline_text="", timeline_media="", timeline_credit="", timeline_caption=""):
+	   self.headline = timeline_headline
+	   self.type = timeline_type
+	   self.text = "<p>%s</p>" % timeline_text
+	   self.asset = {
+								"media": timeline_media,
+								"credit": timeline_credit,
+								"caption": timeline_caption,
+							  }
+	   self.date_list = []
+	   self.era_list = []
+
+	def get_date_list(self):
+	   return self.date_list
+	   
+	def get_era_list(self):
+	   return self.era_list
+	   
+	def add_date(self, headline, start_y, start_m=1, start_d=1, end_y="", end_m=1, end_d=1, text="", tag="", classname="", media="", thumbnail="", credit="", caption=""):
+	   self.date_list.append(self.event(headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d, classname, media, thumbnail, credit, caption))
+	   
+	def add_era(self, headline, start_y, start_m=1, start_d=1, end_y="", end_m=1, end_d=1, text="", tag=""):
+	   self.era_list.append(self.era(headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d))
+	   
+	def timeline2dict(self):
+	   timeline_dict = {"headline": self.headline,
+						"type": self.type,
+						"text": self.text,
+						"asset": self.asset,
+						"date":[],
+						"era":[]}
+	   for date in self.get_date_list():
+			timeline_dict["date"].append(object2dict(date))
+	   for era in self.get_era_list():
+			timeline_dict["era"].append(object2dict(era))
+	   return {"timeline": timeline_dict}
--- Startups/profiles/views.py
+++ Startups/profiles/views.py
@@ -5,6 +5,7 @@ from django.template.loader import get_template
 from django.template import Context
 from django.http import HttpResponse
 from profiles.models import company_ent, funding_ent, acquisition_ent, member_ent
+from profiles import classes
 from django.db.models import Sum
 from time import strptime
 import json
@@ -18,87 +19,19 @@ def profiles_view(request, cid):
 
      #########################
      #retrieve json for graph start here
-     class node(object):
-          def __init__(self, cid, name, match, description, invest_value):
-               self.id = cid #indentified by cid
-               self.name = name
-               self.match = match
-               self.description = description
-               self.invest_value = invest_value
-               self.url = "/profiles/%d" % cid
-          def __repr__(self):
-               return "Node %s with id %d<br>" % (self.name, self.id)
-
-     class edge(object):
-          def __init__(self, source_cid, target_cid):
-               self.source = source_cid   #indentified by cid
-               self.target = target_cid   #indentified by cid
-          def __repr__(self):
-               return "Link from %d to %d<br>" % (self.source, self.target)
-
-     class graph(object):
-          def __init__(self):
-               self.nodes = []
-               self.nodes_id = []
-               self.links = []
-
-          def is_node_exist(self, new_node):
-               return new_node.id in self.nodes_id
-
-          def is_edge_exist(self, new_edge):
-               for link in self.links:
-                    if link.source == new_edge.source and link.target == new_edge.target:
-                         return True
-               else:
-                    return False
-
-          def add_node(self, new_node):
-               if not self.is_node_exist(new_node):
-                    self.nodes.append(new_node)
-                    self.nodes_id.append(new_node.id)
-
-          def add_edge(self, new_edge):
-               if not self.is_edge_exist(new_edge):
-                    self.links.append(new_edge)
-                    
-          def get_nodes(self):
-               return self.nodes
-
-          def get_edges(self):
-               return self.links
-                    
-          def add_investment(self, investor_cid, investment):
-               invest_traget = company_ent.objects.get(pk=investment.cid.cid)
-               #amount should be fixed by partners_count, match haven't been defined yet
-               self.add_node(node(invest_traget.cid, invest_traget.name, 1, invest_traget.short_description, float(investment.amount))) #decimal to float
-               self.add_edge(edge(investor_cid, invest_traget.cid))
-
-          def __repr__(self):
-               message = []
-               message.append("Nodes<br>")
-               for node in self.nodes:
-                    message.append(str(node))
-               message.append("Links<br>")
-               for link in self.links:
-                    message.append(str(link))
-               return "".join(message)
-
-          def graph2dict(self):
-               graph_dict = {"nodes":[], "links":[]}
-               for node in graph.get_nodes():
-                    graph_dict["nodes"].append(object2dict(node))
-               for link in graph.get_edges():
-                    graph_dict["links"].append(object2dict(link))
-               return graph_dict
-
-
      def find_investments_by_cid(cid):
           return funding_ent.objects.filter(investors = company_ent.objects.get(pk=cid).name)
 
      def find_investors_by_cid(cid):
           return funding_ent.objects.filter(cid = cid)
+		  
+     def add_investment(graph, investor_cid, investment):
+          invest_traget = company_ent.objects.get(pk=investment.cid.cid)
+          #amount should be fixed by partners_count, match haven't been defined yet
+          graph.add_node(invest_traget.cid, invest_traget.name, 1, invest_traget.short_description, float(investment.amount)) #decimal to float
+          graph.add_edge(investor_cid, invest_traget.cid)
 
-     graph = graph()
+     graph = classes.graph()
      
      seed_list = [cid,]
      investors_funding = find_investors_by_cid(cid)
@@ -115,13 +48,13 @@ def profiles_view(request, cid):
           if len(investments) > 0:
                invest_sum = investments.aggregate(Sum("amount")).values()
                #add itself as a node
-               graph.add_node(node(seed, company_ent.objects.get(pk=seed).name, 1, company_ent.objects.get(pk=seed).short_description, invest_sum))
+               graph.add_node(seed, company_ent.objects.get(pk=seed).name, 1, company_ent.objects.get(pk=seed).short_description, invest_sum)
           else:
-               graph.add_node(node(seed, company_ent.objects.get(pk=seed).name, 1, company_ent.objects.get(pk=seed).short_description, 1000000)) # default invest_sum 1M
+               graph.add_node(seed, company_ent.objects.get(pk=seed).name, 1, company_ent.objects.get(pk=seed).short_description, 1000000) # default invest_sum 1M
                
           #add other nodes
           for investment in investments:
-               graph.add_investment(seed, investment)
+               add_investment(graph, seed, investment)
 
      
      #retrieve json for graph end here
@@ -129,53 +62,6 @@ def profiles_view(request, cid):
 
      #########################
      #retrieve json for timeline start here
-
-     class era(object):
-          def __init__(self, headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d):
-               self.startDate = "%d,%d,%d" % (start_y, start_m, start_d)
-               if end_y == "":
-                    self.endDate=",,,"
-               else:
-                    self.endDate = "%d,%d,%d" % (end_y, end_m, end_d)
-               self.headline = headline
-               self.text = "<p>%s</p>" % text
-               self.tag = tag
-
-     class event(era):
-          def __init__(self, headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d, classname, media, thumbnail, credit, caption):
-               super(event, self).__init__(headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d)
-               self.classname = classname
-               self.asset = {
-                              "media": media,
-                              "thumbnail": thumbnail,
-                              "credit": credit,
-                              "caption": caption,
-                            }
-
-     class timeline(object):
-          def __init__(self, timeline_headline, timeline_type="default", timeline_text="", timeline_media="", timeline_credit="", timeline_caption=""):
-               self.headline = timeline_headline
-               self.type = timeline_type
-               self.text = "<p>%s</p>" % timeline_text
-               self.asset = {
-                                        "media": timeline_media,
-                                        "credit": timeline_credit,
-                                        "caption": timeline_caption,
-                                      }
-               self.date = []
-               self.era = []
-
-          def add_date(self, headline, start_y, start_m=1, start_d=1, end_y="", end_m=1, end_d=1, text="", tag="", classname="", media="", thumbnail="", credit="", caption=""):
-               self.date.append(event(headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d, classname, media, thumbnail, credit, caption))
-               
-          def add_era(self, headline, start_y, start_m=1, start_d=1, end_y="", end_m=1, end_d=1, text="", tag=""):
-               self.era.append(era(headline, text, tag, start_y, start_m, start_d, end_y, end_m, end_d))
-               
-          def timeline2dict(self):
-               timeline_dict = object2dict(timeline)
-               return {"timeline": timeline_dict}
-
-     timeline = timeline(timeline_headline = "Milestones of %s" % company_ent.objects.get(pk=seed).name, timeline_text="with timeline display")
      
      #Date reader for acquisition
      def read_date(string):
@@ -188,6 +74,8 @@ def profiles_view(request, cid):
                result = strptime(string, "%Y")
           return result
 
+     timeline = classes.timeline(timeline_headline = "Milestones of %s" % company_ent.objects.get(pk=seed).name, timeline_text="with timeline display")
+		  
      #Funding events
      queryset = funding_ent.objects.filter(cid = cid)
      
@@ -222,31 +110,10 @@ def profiles_view(request, cid):
 
      #retrieve json for timeline end here
      ######################### 
-
-     def object2dict(obj):
-          #convert object to a dict
-          d = {}
-          #d['__class__'] = obj.__class__.__name__
-          #d['__module__'] = obj.__module__
-          d.update(obj.__dict__)
-          return d
-
-##     def dict2object(d):
-##          #convert dict to object
-##          if'__class__' in d:
-##               class_name = d.pop('__class__')
-##               module_name = d.pop('__module__')
-##               module = __import__(module_name)
-##               class_ = getattr(module,class_name)
-##               args = dict((key.encode('ascii'), value) for key, value in d.items()) #get args
-##               inst = class_(**args) #create new instance
-##          else:
-##               inst = d
-##          return inst
      
      
      t = get_template('profile.html')
-     html = t.render(Context({'company':company_ent.objects.get(pk=cid), 'json': json.dumps(graph,default=object2dict), 'timeline': json.dumps(timeline.timeline2dict(),default=object2dict)}))
+     html = t.render(Context({'company':company_ent.objects.get(pk=cid), 'json': json.dumps(graph.graph2dict()), 'timeline': json.dumps(timeline.timeline2dict())}))
      return HttpResponse(html)
 
 	 
